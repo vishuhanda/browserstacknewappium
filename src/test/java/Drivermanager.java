5928c4d1cf4c5b5c455d4c5f37e953eb095ae878
vishu <itsvishuhanda@gmail.com>
2021-07-12 22:18:37 +0530

    browser stack functionality added

--- src/test/java/Drivermanager.java
+++ src/test/java/Drivermanager.java
@@ -2,18 +2,9 @@
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import io.appium.java_client.MobileElement;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -31,10 +22,10 @@ public class Drivermanager {
     public static void initDriver() throws MalformedURLException {
 
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("app", "bs://bd83683ba0ff3fa2c3c2d247c2ca0d6e64058c94");
+        caps.setCapability("app", System.getProperty("browserstackurl"));
         caps.setCapability("browserstack.user", "vhvl_9KOMkx");
         caps.setCapability("browserstack.key", "puL2x1SZ4AhecfrcxW39");
-
+        System.out.println("browserstackurl :- " + System.getProperty("browserstackurl"));
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");
         caps.setCapability("os_version", "9.0");
